02425619b5ba6a35b51b12c44cbdfb57c4a13ca9
Karen Araqelyan <karen.araqelyan@hylink.am>
2025-08-06 15:50:46 +0400

    add functionality

---
+++ lib/empty_list_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EmptyListWidget extends StatelessWidget {
+  const EmptyListWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Icon(Icons.list, size: 80));
+  }
+}
